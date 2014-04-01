5f25bae25c560fd877c83e6523e7b92d5a96e54e
David Chavez <david@psychosity.net>
2014-04-01 10:09:07 -0500

    added Bond class

--- src/Bond.java
+++ src/Bond.java
@@ -1,16 +1,12 @@
 //
-// Rule.java
+// Bond.java
 // Nubot Simulator
 //
 // Created by David Chavez on 4/1/14.
 // Copyright (c) 2014 Algorithmic Self-Assembly Research Group. All rights reserved.
 //
 
-import java.awt.*;
-
-public class Rule
+public class Bond
 {
-    String s1, s2, s1p, s2p;
-    Bond start, end;
-    Point dir, dirp;
+
 }
