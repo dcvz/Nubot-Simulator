d82ddd7fe20df48ee30dc67c8ac04e1dc13aedf0
David Chavez <david@psychosity.net>
2014-04-01 10:35:18 -0500

    finished Monomer class & added javatuples library

--- src/Monomer.java
+++ src/Monomer.java
@@ -6,7 +6,24 @@
 // Copyright (c) 2014 Algorithmic Self-Assembly Research Group. All rights reserved.
 //
 
+import java.awt.*;
+
 public class Monomer
 {
+    private Point location;
+    private String state;
 
+    public Monomer(Point p, String s)
+    {
+        this.location = p;
+        this.state = s;
+    }
+
+    // accessor methods
+    public Point getLocation() { return location; }
+    public String getState() { return state; }
+
+    // mutator methods
+    public void setLocation(Point p) { this.location= p; }
+    public void setState(String s) { this.state = s; }
 }
